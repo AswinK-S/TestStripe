bff6f9b108120236cfb4d29477ef26334f3df112
Aswin K S <aswinkalathur11@gmail.com>
2024-04-07 10:50:14 +0530

    react-stripe-checkout setup done in frontend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
     const express = require('express')
     const cors = require('cors')
+    require('dotenv').config()
 
-    // const stripe = require('stripe')('')
+    const stripe = require('stripe')(process.env.stripe_secret_key)
     const {v4:uuidv4} = require('uuid')
 
     const app=express()
@@ -11,8 +12,39 @@
     app.use(cors())
 
     //routes
+
+
     app.get('/',(req,res)=>{
         res.send('Working')
+    })
+
+    app.post('/payment',(req,res)=>{
+        const {product , token}= req.body
+
+        const ideompotencyKey = uuid()
+
+        return stripe.customers.create({
+            email:token.email,
+            source:token.id
+        }).then(customer =>{
+            stripe.charges.create({
+                amount: product.price*100,
+                currency : 'usd',
+                customer :customer.id,
+                receipt_email:token.email,
+                shipping:{
+                    name: token.card.name,
+                    address:{
+                        country: token.card.address_country
+                    }
+                }
+            },{ideompotencyKey})
+        }).then(result =>{
+            res.status(200).json(result)
+        }).catch(err=>{
+            console.log(err.message);
+        })
+
     })
 
     //listen
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
-
+import { useState } from "react"
+import StripeCheckout from 'react-stripe-checkout'
 function App() {
 
+  const [product, setProduct] = useState({
+    name: 'stripe',
+    price: 20,
+    productBy: 'Stripe'
+  })
+
+
   return (
+
+
     <>
-      
-        
+      <div className="bg-black h-screen flex justify-center items-center ">
+        <StripeCheckout stripeKey="" name="" token='' />
+      </div>
+
     </>
   )
 }
